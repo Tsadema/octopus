3eeb3d27923e3732f5de65a1a94daeef7a687254
Tsadema <986angryhello2@gmail.com>
2021-05-05 14:06:54 -0700

    Added dice command

---
+++ commands/fun/dice.js
@@ -0,0 +1,35 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class DiceCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'dice',
+            aliases: ['roll'],
+            group: 'fun',
+            memberName: 'dice',
+            description: 'roll a standard 1-6 dice',
+            clientPermissions: ['SEND_MESSAGES'],
+            args: [
+                {
+                    key: 'minimum',
+                    prompt: 'minimum number for dice roll',
+                    type: 'integer',
+                    'default': '1',
+                },
+                {
+                    key: 'maximum',
+                    prompt: 'maximum number for dice roll',
+                    type: 'integer',
+                    'default': '6',
+                },
+            ],
+        });
+    }
+
+    run(message, { minimum, maximum }) {
+        let min = Math.ceil(minimum);
+        let max = Math.floor(maximum + 1);
+        let number = (Math.floor(Math.random() * (max - min) + min));
+        return message.say(`You rolled ... ${number}!`)
+    };
+};
