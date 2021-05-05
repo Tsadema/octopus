b0a2b60cc3329414d422eea1cb21add270783316
Tsadema <986angryhello2@gmail.com>
2021-05-05 13:05:53 -0700

    Initial commit x2

---
+++ commands/fun/say.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class SayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'say',
+            aliases: ['parrot', 'copy'],
+            group: 'fun',
+            memberName: 'say',
+            description: 'what u want me 2 say?',
+            clientPermissions: ['SEND_MESSAGES'],
+            args: [
+                {
+                    key: 'text',
+                    prompt: 'dafuq you want me to say',
+                    type: 'string',
+                    validate: text => text.length < 201,
+                },
+            ],
+        });
+    }
+
+    run(message, { text }) {
+        return message.reply(text);
+    }
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+const chalk = require('chalk');
+const { token } = require('./config.json');
+
+const client = new CommandoClient({
+    commandPrefix: '8!',
+    owner: '414599338908450817',
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['general', 'General utility commands'],
+        ['fun', 'Fun commands'],
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+    console.log(chalk.blueBright(`\nLogged in as ${client.user.tag}\n`))
+});
+
+client.on('error', chalk.redBright(console.error));
+
+
+
+
+
+client.login(token);
